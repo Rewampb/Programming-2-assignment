2c979c30803c91411c202e0dfb472794b150cfe4
Rewampb <worrawat.panton@gmail.com>
2025-02-23 09:36:30 +0100

    Update task1.mjs

--- moduleSkillSummary_assignment-main/example_files/task1.mjs
+++ moduleSkillSummary_assignment-main/example_files/task1.mjs
@@ -2,3 +2,9 @@
 function square(number) {
     return number * number;
 }
+
+//A function that returns a length in mm assuming it has been given a length in inches.
+function inchesToMillemeters(inches) {
+    const conversionRate = 25.4;
+    return inches * conversionRate;
+}
